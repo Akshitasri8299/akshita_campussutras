181a223e4240fae224614e793f3f42fbf7827471
Akshita Srivastava <your-github-email>
2026-06-18 22:41:05 +0530

    update

---
+++ p2.js
@@ -0,0 +1,13 @@
+/*
+Check if number is positive,negative or zero
+*/
+let n=6;
+if(n>0){
+    console.log("number is positive");
+}
+else if(n<0){
+    console.log("number is negative");
+}
+else{
+    console.log("number is zero");
+}
---
+++ p3.js
@@ -0,0 +1,11 @@
+/*
+find largest of two numbers
+*/
+let a=19;
+let b=4;
+if(a>b){
+    console.log("a is largest");
+}
+else{
+    console.log("b is largest");
+}
