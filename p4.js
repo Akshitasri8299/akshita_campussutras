9cd38b0004598104bffb77474bbb84ef27ab010b
Akshita Srivastava <your-github-email>
2026-06-18 23:37:36 +0530

    ALL THE FILES ARE MODIFIED

---
+++ p4.js
@@ -0,0 +1,15 @@
+/*
+Find the largest of three numbers
+*/
+let l=8;
+let m=6;
+let k=4;
+if(l>m && l>k){
+    console.log("l is largest");
+}
+else if(m>l && m>k){
+    console.log("m is largest");
+}
+else{
+    console.log("k is largest");
+}
---
+++ p5.js
@@ -0,0 +1,10 @@
+/*
+Check divisibility by 3 and 5
+*/
+n=15;
+if(n%3==0 && n%5==0){
+    console.log("Divisible by both 3 and 5");
+}
+else{
+    console.log("Not divisible by both 3 and 5");
+}
